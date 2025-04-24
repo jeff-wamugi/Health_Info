396a3739fd085eabc41f36b5cacfb23fb00e2432
jeff-wamugi <78215278+jeff-wamugi@users.noreply.github.com>
2025-04-24 16:48:14 +0300

    created endpoints for each view set

--- api/views.py
+++ api/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status, viewsets, generics, filters
+from .models import Program, Client, Enrollment
+from .serializers import ProgramSerializer, ClientSerializer, EnrollmentSerializer, ClientProfileSerializer
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
+# This is a Django view that handles HTTP requests for the health information system.
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'contact']
+    permission_classes = [AllowAny]
+
+class ProgramViewSet(viewsets.ModelViewSet):
+    queryset = Program.objects.all()
+    serializer_class = ProgramSerializer
+
+class EnrollmentViewSet(viewsets.ModelViewSet):
+    queryset = Enrollment.objects.all()
+    serializer_class = EnrollmentSerializer
+
+class ClientProfileView(generics.RetrieveAPIView):
+    queryset = Client.objects.all()
+    serializer_class = ClientProfileSerializer
