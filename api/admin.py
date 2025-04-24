98428c33620121a5f25020444988ab0d8a53eb05
jeff-wamugi <78215278+jeff-wamugi@users.noreply.github.com>
2025-04-24 16:45:21 +0300

    registered models created

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Program, Client, Enrollment
 
 # Register your models here.
+admin.site.register(Program)
+admin.site.register(Client)
+admin.site.register(Enrollment)
+# Compare this snippet from api/views.py:
+# from django.shortcuts import render
+# from django.http import JsonResponse
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+# from rest_framework import status
+# from .models import Program, Client, Enrollment
+# from .serializers import ProgramSerializer, ClientSerializer, EnrollmentSerializer
+  
