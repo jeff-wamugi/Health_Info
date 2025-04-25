fcb609c239f9c517696943a988c1d379ab1dcd06
jeff-wamugi <78215278+jeff-wamugi@users.noreply.github.com>
2025-04-25 14:29:06 +0300

    swapped lines 23 and 24

--- healthinfo/urls.py
+++ healthinfo/urls.py
@@ -20,8 +20,8 @@ from django.conf import settings
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
