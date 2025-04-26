33e85cc8134c6aa02a0982e1b5036c5a500650ea
jeff-wamugi <78215278+jeff-wamugi@users.noreply.github.com>
2025-04-26 21:36:42 +0300

    fixed import error

--- healthinfo-frontend/src/pages/Clients.js
+++ healthinfo-frontend/src/pages/Clients.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import api from '../api/api';
-import useNavigate from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function Clients() {
   const [name, setName] = useState('');
